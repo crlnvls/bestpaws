5f670c126d7f48680611a88c6792a97564388eb6
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-09 22:35:19 +0000

    Create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import {
+  Homepage,
+  About,
+  Adoption,
+  Care,
+  Login,
+  Register,
+  Cats,
+  Dogs,
+  NotFound,
+} from "./pages/Homepage";
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>Best Paws</h1>}></Route>
-      <Route path="/about-us" element={<h1>About</h1>}></Route>
-      <Route path="/paws-adoption" element={<h1>Paws Adoption</h1>}></Route>
-      <Route path="/paws-care" element={<h1>Paws Care</h1>}></Route>
-      <Route path="/login" element={<h1>Login</h1>}></Route>
-      <Route path="/register" element={<h1>Register</h1>}></Route>
-      <Route path="/cats" element={<h1>Cats</h1>}></Route>
-      <Route path="/dogs" element={<h1>Dogs</h1>}></Route>
-      <Route path="*" element={<h1>Page not found</h1>}></Route>
+      <Route path="/" element={<Homepage />}></Route>
+      <Route path="/about-us" element={<About />}></Route>
+      <Route path="/paws-adoption" element={<Adoption />}></Route>
+      <Route path="/paws-care" element={<Care />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/cats" element={<Cats />}></Route>
+      <Route path="/dogs" element={<Dogs />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
 };
