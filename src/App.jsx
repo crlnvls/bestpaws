b2f2e27b8a1bf288b636e3196dca98c14b04062a
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-09 22:09:58 +0000

    Add more routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<h1>Best Paws</h1>}></Route>
+      <Route path="/about" element={<h1>About</h1>}></Route>
+      <Route path="/paws-adoption" element={<h1>Paws Adoption</h1>}></Route>
+      <Route path="/login" element={<h1>Login</h1>}></Route>
+      <Route path="/register" element={<h1>Register</h1>}></Route>
       <Route path="/cats" element={<h1>Cats</h1>}></Route>
       <Route path="/dogs" element={<h1>Dogs</h1>}></Route>
     </Routes>
