e3f988e0ce97c4fd09b61b7de8f83f7232157799
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-09 22:20:52 +0000

    Add route 'page not found'

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<h1>Best Paws</h1>}></Route>
-      <Route path="/about" element={<h1>About</h1>}></Route>
+      <Route path="/about-us" element={<h1>About</h1>}></Route>
       <Route path="/paws-adoption" element={<h1>Paws Adoption</h1>}></Route>
+      <Route path="/paws-care" element={<h1>Paws Care</h1>}></Route>
       <Route path="/login" element={<h1>Login</h1>}></Route>
       <Route path="/register" element={<h1>Register</h1>}></Route>
       <Route path="/cats" element={<h1>Cats</h1>}></Route>
       <Route path="/dogs" element={<h1>Dogs</h1>}></Route>
+      <Route path="*" element={<h1>Page not found</h1>}></Route>
     </Routes>
   );
 };
