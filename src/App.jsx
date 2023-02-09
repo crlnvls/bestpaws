de1eafb3adf9601e300f82b3421d27e91559c237
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-09 22:05:33 +0000

    Fix error head index.html and routes App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-const App = () => {};
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Best Paws</h1>}></Route>
+      <Route path="/cats" element={<h1>Cats</h1>}></Route>
+      <Route path="/dogs" element={<h1>Dogs</h1>}></Route>
+    </Routes>
+  );
+};
 
 export default App;
